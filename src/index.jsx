6539afac02a2ccf90a4403f2702df277e2a4770a
CamiloAliaga295 <114539521+CamiloAliaga295@users.noreply.github.com>
2023-01-31 18:04:39 -0300

    Add files via upload
    "se agrega index.jsx de la card"

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,21 @@
-import {Container,Button} from 'react-bootstrap' 
-import './footer.css'
+import {Button, Card} from 'react-bootstrap';
 
-const Footer = () => {
+const CardImage = props => {
+
+    const {title,coment,src } = props
+
+
     return (
-<Container className= 'footer justify-content text-center'>
-
-    <h1>Todos los derechos reservados</h1>
-        <Button className='primary'>Siguenos aca</Button>
-</Container>
-
+        <Card style={{ width: '18rem' }}>
+        <Card.Img  variant="top" src={src}/>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>
+            {coment}
+          </Card.Text>
+        </Card.Body>
+      </Card>
     )
 }
 
-export default Footer
+export default CardImage
